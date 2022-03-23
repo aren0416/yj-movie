7625f0d75caf9ce7fa1bdd572d4d8d2d785eb1be
aren0416 <aren0416@gmail.com>
2022-03-23 21:59:21 +0900

    api setting - axios 사용

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "5cc72a8f41eb9b91b2f02e2aca853617",
+    language: "ko",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  popular: () => api.get("movie/popular"),
+};
