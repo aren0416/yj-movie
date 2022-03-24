96265dea8382deff0aa2bc6b9881a52a818b9a08
aren0416 <aren0416@gmail.com>
2022-03-24 21:31:41 +0900

    useEffec 사용  async와 await

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,18 @@
 import { PageTitle } from "../PageTitle";
 import { movieApi } from "../../api";
+import { useEffect } from "react";
 
-console.log(movieApi.nowPlaying());
+//console.log(movieApi.nowPlaying());
 
 export const Home = () => {
+  useEffect(() => {
+    const movieData = async () => {
+      const nowPlayingData = await (await movieApi.nowPlaying()).data.results;
+      console.log(nowPlayingData);
+    };
+    movieData();
+  }, []);
+
   return (
     <>
       <PageTitle title={"Home"} />
