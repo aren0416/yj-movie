4f9cb04944944380ba1b8d4ee0ef9b0a76c017a8
aren0416 <aren0416@gmail.com>
2022-03-30 20:54:50 +0900

    현재상영영화 페이지 분리(Movies.js)

---
+++ src/components/home/Movies.js
@@ -0,0 +1,55 @@
+import { mainTitle } from "../styes/GlobalStyled";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import { Link } from "react-router-dom";
+import { routes } from "../../routes";
+
+const MovieWrap = styled.div`
+  margin-top: 100px;
+`;
+
+const Title = styled.h3`
+  font-size: ${mainTitle.fontSize};
+  font-weight: ${mainTitle.fontWeight};
+  margin-bottom: 30px;
+`;
+
+const Img = styled.div`
+  width: 100%;
+  height: 200px;
+`;
+
+const MovieTitle = styled.h4`
+  font-size: 18px;
+  margin-top: 15px;
+`;
+
+SwiperCore.use([Navigation]);
+
+const params = {
+  spaceBetween: 5.2,
+  slidesPerView: 5.5,
+};
+
+export const Movies = ({ movieData }) => {
+  return (
+    <MovieWrap>
+      <Title>현재 상영 영화</Title>
+      <Swiper modules={[Navigation]} {...params}>
+        {movieData.map((now) => (
+          <SwiperSlide key={now.id}>
+            <Link to={`${routes.detail}/${now.id}`}>
+              <Img
+                style={{
+                  background: `url(https://image.tmdb.org/t/p/original${now.backdrop_path}) no-repeat center / cover`,
+                }}
+              />
+              <MovieTitle>{now.title}</MovieTitle>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </MovieWrap>
+  );
+};
